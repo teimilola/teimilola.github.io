05ed4f29bf72f9b752a1d03336a8514e723ff0e0
mah343 <mah343@COECIS.local>
2016-10-02 12:45:15 -0400

    ...

--- website.py
+++ website.py
@@ -1,7 +1,12 @@
+from flask import Flask
+import os
 
-m flask import Flask
 app = Flask(__name__)
 
 @app.route('/')
 def index():
     return 'Index Page'
+
+if __name__ == '__main__':
+    port = int(os.environ.get('PORT', 5000))
+    app.run(host='0.0.0.0', port=port)
